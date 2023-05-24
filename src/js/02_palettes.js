3d7b59a4ba223a61a0ed604a2580340721d24dd0
MiriamPaternain <miriampaternain@gmail.com>
2023-05-24 19:20:21 +0200

    add js palettes

---
+++ src/js/02_palettes.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const radio1 = document.querySelector('.js_radio1');
+const radio2 = document.querySelector('.js_radio2');
+const radio3 = document.querySelector('.js_radio3');
+const previewCard = document.querySelector('.js_preview__card');
+
+function handleClickRadio1 (event){
+  previewCard.classList.remove('palette2', 'palette3');
+  previewCard.classList.add('palette1');
+}
+
+radio1.addEventListener('click', handleClickRadio1);
+
+function handleClickRadio2 (event){
+  previewCard.classList.remove('palette1', 'palette3');
+  previewCard.classList.add('palette2');
+}
+
+radio2.addEventListener('click', handleClickRadio2);
+
+function handleClickRadio3 (event){
+  previewCard.classList.remove('palette1', 'palette2');
+  previewCard.classList.add('palette3');
+}
+
+radio3.addEventListener('click', handleClickRadio3);
+
+console.log(hola);
