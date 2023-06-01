b6c200311d452c36a2b042cefebf99e8b62297a3
MariaPeCa <mpenacantero@gmail.com>
2023-06-01 13:51:50 +0200

    activate reset btn

---
+++ src/js/04_reset.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const resetButton = document.getElementById('reset');
+
+// Agrega un evento click al botón
+resetButton.addEventListener('click', function () {
+// Recarga la página completa
+  location.reload();
+});
+
+
