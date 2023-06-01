352fc9fd990ab0b941b9b4bdbc7e66d99de6895e
MiriamPaternain <miriampaternain@gmail.com>
2023-06-01 19:08:34 +0200

    add changes

--- src/js/03_form.js
+++ src/js/03_form.js
@@ -19,7 +19,7 @@ const inputEmail = document.querySelector ('.js_input_email');
 const previewEmail = document.querySelector ('.rrss__link--email');
 function handleInputEmail(event){
     const emailValue = event.target.value;
-    previewEmail.href = emailValue;
+    previewEmail.href.innerHTML = emailValue;  //he añadido inner.html, ahora no da error en el navegardor y se abre la app de envio de mails
 }
 inputEmail.addEventListener ('input',handleInputEmail); 
 
@@ -29,13 +29,13 @@ function handleInputTel(event){
     const telValue = event.target.value;
     previewTel.href = telValue;
 }
-inputEmail.addEventListener ('input',handleInputTel); 
+inputTel.addEventListener ('input',handleInputTel);  //he cambiado el inputMail del evento por el inputTel, ahora no resetea la pag
 
 const inputLinkedin = document.querySelector ('.js_input_linkedin');
 const previewLinkedin = document.querySelector ('.rrss__link--linkedin');
 function handleInputLinkedin(event){
     const linkedinValue = event.target.value;
-    previewLinkedin.html = linkedinValue;
+    previewLinkedin.href = linkedinValue; //he sustituido html (previewLinkedin.html )por href
 }
 inputLinkedin.addEventListener ('input',handleInputLinkedin); 
 
@@ -43,6 +43,7 @@ const inputGithub = document.querySelector ('.js_input_github');
 const previewGithub = document.querySelector ('.rrss__link--github');
 function handleInputGithub(event){
     const githubValue = event.target.value;
-    previewGithub.html = githubValue;
+    previewGithub.href.innerHTML = githubValue;
 }
-inputGithub.addEventListener ('input',handleInputGithub);
+inputGithub.addEventListener ('input', handleInputGithub);
+
