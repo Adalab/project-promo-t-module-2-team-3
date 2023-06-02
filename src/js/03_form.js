fd5c89187f7068a4f1ab6957b3b2b6b6b598f78b
Jenifer <jaldo.jeny@gmail.com>
2023-06-02 14:21:46 +0200

    campos obligatorios

--- src/js/03_form.js
+++ src/js/03_form.js
@@ -1,48 +1,61 @@
-'use strict'
-const inputName = document.querySelector ('.js_input_name');
-const previewName = document.querySelector ('.card__info--name');
-function handleInputName(event){
-    const inputValue = event.target.value;
-    previewName.innerHTML= inputValue;
+'use strict';
+const inputName = document.querySelector('.js_input_name');
+const previewName = document.querySelector('.card__info--name');
+function handleInputName(event) {
+  const inputValue = event.target.value;
+  previewName.innerHTML = inputValue;
 }
-inputName.addEventListener ('input',handleInputName);
+inputName.addEventListener('input', handleInputName);
 
-const inputJob = document.querySelector ('.js_input_job');
-const previewJob = document.querySelector ('.card__info--job');
-function handleInputJob(event){
-    const jobValue = event.target.value;
-    previewJob.innerHTML=jobValue;
+const inputJob = document.querySelector('.js_input_job');
+const previewJob = document.querySelector('.card__info--job');
+function handleInputJob(event) {
+  const jobValue = event.target.value;
+  previewJob.innerHTML = jobValue;
 }
-inputJob.addEventListener ('input',handleInputJob);
+inputJob.addEventListener('input', handleInputJob);
 
-const inputEmail = document.querySelector ('.js_input_email');
-const previewEmail = document.querySelector ('.rrss__link--email');
-function handleInputEmail(event){
-    const emailValue = event.target.value;
-    previewEmail.href = emailValue;
+const inputEmail = document.querySelector('.js_input_email');
+const previewEmail = document.querySelector('.rrss__link--email');
+function handleInputEmail(event) {
+  const emailValue = event.target.value;
+  previewEmail.href = emailValue;
 }
-inputEmail.addEventListener ('input',handleInputEmail); 
+inputEmail.addEventListener('input', handleInputEmail);
 
-const inputTel = document.querySelector ('.js_input_tel');
-const previewTel = document.querySelector ('.rrss__link--tel');
-function handleInputTel(event){
-    const telValue = event.target.value;
-    previewTel.href = telValue;
+const inputTel = document.querySelector('.js_input_tel');
+const previewTel = document.querySelector('.rrss__link--tel');
+function handleInputTel(event) {
+  const telValue = event.target.value;
+  previewTel.href = telValue;
 }
-inputEmail.addEventListener ('input',handleInputTel); 
+inputEmail.addEventListener('input', handleInputTel);
 
-const inputLinkedin = document.querySelector ('.js_input_linkedin');
-const previewLinkedin = document.querySelector ('.rrss__link--linkedin');
-function handleInputLinkedin(event){
-    const linkedinValue = event.target.value;
-    previewLinkedin.html = linkedinValue;
+const inputLinkedin = document.querySelector('.js_input_linkedin');
+const previewLinkedin = document.querySelector('.rrss__link--linkedin');
+function handleInputLinkedin(event) {
+  const linkedinValue = event.target.value;
+  previewLinkedin.html = linkedinValue;
 }
-inputLinkedin.addEventListener ('input',handleInputLinkedin); 
+inputLinkedin.addEventListener('input', handleInputLinkedin);
 
-const inputGithub = document.querySelector ('.js_input_github');
-const previewGithub = document.querySelector ('.rrss__link--github');
-function handleInputGithub(event){
-    const githubValue = event.target.value;
-    previewGithub.html = githubValue;
+const inputGithub = document.querySelector('.js_input_github');
+const previewGithub = document.querySelector('.rrss__link--github');
+function handleInputGithub(event) {
+  const githubValue = event.target.value;
+  previewGithub.html = githubValue;
 }
-inputGithub.addEventListener ('input',handleInputGithub);
+inputGithub.addEventListener('input', handleInputGithub);
+
+const createButton = document.querySelector('.js_createbutton');
+const createError = document.querySelector('.js - create - error');
+function handleCreateButton(event) {
+  if (
+    inputName === '' ||
+    inputJob === '' ||
+    inputTel === '' ||
+    inputEmail === ''
+  ) {
+    createError.innerHTML = 'Rellena todos los campos obligatorios';
+  }
+}
