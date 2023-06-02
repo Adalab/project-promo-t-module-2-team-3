a9e8b5f1efbef0188a2e4df8b689f02368f15ea1
MariaPeCa <mpenacantero@gmail.com>
2023-06-02 19:50:55 +0200

    reset btn

--- src/js/04_reset.js
+++ src/js/04_reset.js
@@ -17,7 +17,23 @@ const handleClickReset = (event) => {
   inputGithub.value = '';
   cardName.innerHTML = 'Nombre Apellido';
   cardJob.innerHTML = 'Front-end developer';
+  designContainer.classList.remove('collapsed');
+  fillContainer.classList.add('collapsed');
+  shareContainer.classList.add('collapsed');
+  previewCard.classList.add('palette1');
+  /*
+  designLegend.addEventListener('click', (event) => {
+    designContainer.classList.remove('collapsed');
+    fillContainer.classList.add('collapsed');
+    shareContainer.classList.add('collapsed');
+    rotateArrow();
+  });
+  
+  previewCard.classList.remove('palette2', 'palette3');
+  previewCard.classList.add('palette1');
+*/
 };
+
 
 resetButton.addEventListener('click', handleClickReset);
 
