94203c4e46ecac41af5e492734f48107e65450f5
MariaPeCa <mpenacantero@gmail.com>
2023-06-05 14:04:49 +0200

    reset form and add checked to radio

--- src/js/04_reset.js
+++ src/js/04_reset.js
@@ -8,7 +8,8 @@ const cardJob = document.querySelector('.js_card-job');
 
 const handleClickReset = (event) => {
   event.preventDefault();
-
+  const radio1 = document.querySelector('.js_radio1');
+  const previewCard = document.querySelector('.js_preview__card');
   inputName.value = '';
   inputJob.value = '';
   inputEmail.value = '';
@@ -20,6 +21,7 @@ const handleClickReset = (event) => {
   designContainer.classList.remove('collapsed');
   fillContainer.classList.add('collapsed');
   shareContainer.classList.add('collapsed');
+  radio1.setAttribute('checked','checked');
   previewCard.classList.add('palette1');
   /*
   designLegend.addEventListener('click', (event) => {
